cd7799566c454296ffe52ecc7cce2a2ea0f49294
Siddharth Jha <SidJha03@gmail.com>
2020-06-01 13:30:19 -0400

    Fixed Settings

--- YPA/settings.py
+++ YPA/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('YPA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('YPA_DEBUG_VALUE') == 'True'
 
 ALLOWED_HOSTS = ['*', 'https://ypaphysics.herokuapp.com/']
 
